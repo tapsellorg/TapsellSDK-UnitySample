9eb1c4ea29c68472f717e7f4aad7063e19821b80
behdad222 <behdad.222@gmail.com>
2019-05-08 21:10:59 +0430

    add scenes

---
+++ Assets/FirstScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstScene : MonoBehaviour {
+
+  void Start () { }
+
+  public void changeScenes (string name) {
+    SceneManager.LoadScene (name);
+  }
+}
